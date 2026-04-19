1f22e1c71d7f1212c255bec5d99ce744205fcfc6
Kuni-02 <mio180221@gmail.com>
2026-04-19 21:30:14 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
+from flask import Flask,request,render_template,url_for
+app=Flask(__name__)
 
+@app.route("/")
+def login():
+    return render_template("login.html")
+
+@app.route("/new")
+def new():
+    return render_template("makeacc.html")
+    
+if __name__=="__main__":
+    app.run(debug=True)
